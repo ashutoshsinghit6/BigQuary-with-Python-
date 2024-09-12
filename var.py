8ad37312cd31fe062a443a68998b9f8df274b235
Ashutosh kumar singh <68853828+ashutoshsinghit6@users.noreply.github.com>
2024-09-12 16:11:29 +0530

    Add files via upload

---
+++ var.py
@@ -0,0 +1,7 @@
+#Add variable values as per your requirements
+
+project = "hive-project"
+my_dataset = "py_dataset"
+my_table = "py_table"
+src_bucket = "bkt-dev-src"
+destination_bucket = "bkt-dev-dst"
